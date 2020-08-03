2ad58ce9b1b6cd3a24f3673f036f930e3d1b4d21
dl769 <qwertyx7xx@gmail.com>
2020-08-04 00:02:43 +0200

    Updates

---
+++ precache-manifest.613c7e6e8c30f255a83209b6e3824a85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138563b16dc586d0fc46c18132b7cf33",
+    "url": "/youtubeDownload/index.html"
+  },
+  {
+    "revision": "9eee17b9cb1fa180b794",
+    "url": "/youtubeDownload/static/css/main.850bc89d.chunk.css"
+  },
+  {
+    "revision": "bfcfaa0e934c14505845",
+    "url": "/youtubeDownload/static/js/2.7164655f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtubeDownload/static/js/2.7164655f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eee17b9cb1fa180b794",
+    "url": "/youtubeDownload/static/js/main.3ac7dec3.chunk.js"
+  },
+  {
+    "revision": "5607b3c94730168fa4e8",
+    "url": "/youtubeDownload/static/js/runtime-main.0eaa7208.js"
+  },
+  {
+    "revision": "be8354c394a3e6388187694777aa94c7",
+    "url": "/youtubeDownload/static/media/bg.be8354c3.jpg"
+  }
+]);
